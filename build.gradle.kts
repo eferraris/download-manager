2049a491c338529f3ad47dd493b0852290da2f74
eferraris <enrique.ferraris@zoomagri.com>
2022-02-17 15:43:16 -0300

    ADD - package creation.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.6.10"
+    `maven-publish` apply false
 }
 
 group="com.eferraris"
@@ -16,4 +17,28 @@ dependencies {
     implementation("commons-io:commons-io:2.11.0")
     implementation("commons-io:commons-io:2.11.0")
     implementation("org.slf4j:slf4j-simple:1.7.36")
+}
+
+subprojects {
+    apply(plugin = "maven-publish")
+    configure<PublishingExtension> {
+        repositories {
+
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/eferraris/download-manager")
+                credentials {
+                    username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                    password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+                }
+            }
+        }
+
+        publications {
+            register<MavenPublication>("gpr") {
+                from(components["java"])
+            }
+        }
+
+    }
 }
